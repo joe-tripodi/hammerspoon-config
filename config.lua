2cd60083bacd9cac05686c91a13728777a6dd584
Joe Tripodi <au.joseph.tripodi@gmail.com>
2026-05-20 08:39:22 +0200

    4 quad placement

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
--- Set current window to full left 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
+-- Set current window to full left
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "Left", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
@@ -13,7 +13,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
 end)
 
 -- Set current window to full right
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "Right", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
@@ -27,7 +27,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
 end)
 
 -- Set current window to full screen
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", function()
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "Up", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
@@ -41,7 +41,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", function()
 end)
 
 -- Put window on bottom of screen
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", function()
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "Down", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
@@ -54,8 +54,8 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", function()
   win:setFrame(f)
 end)
 
--- Set current window to half left 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, ",", function()
+-- Set current window to half left
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, ",", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
@@ -69,15 +69,91 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, ",", function()
 end)
 
 -- Set current window to middle
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Space", function()
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "Space", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
   local max = screen:frame()
 
-  f.x = max.x + (max.w / 4)
+  f.x = max.x + (max.w / 6)
+  f.y = max.y + max.y / 3
+  f.w = (2 * max.w) / 3
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Set current window to middle, but small
+-- Good for Anki
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "m", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + max.w / 3
+  f.y = max.y + max.h / 6
+  f.w = max.w / 3
+  f.h = (2 * max.h) / 3
+  win:setFrame(f)
+end)
+
+
+-- [x] [ ]
+-- [ ] [ ]
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "u", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
   f.y = max.y
   f.w = max.w / 2
-  f.h = max.h
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+-- [ ] [ ]
+-- [x] [ ]
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "i", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y + max.h / 2
+  f.w = max.w / 2
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+-- [ ] [x]
+-- [ ] [ ]
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "p", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + max.w / 2
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+-- [ ] [ ]
+-- [ ] [x]
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "o", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + max.w / 2
+  f.y = max.y + max.h / 2
+  f.w = max.w / 2
+  f.h = max.h / 2
   win:setFrame(f)
 end)
